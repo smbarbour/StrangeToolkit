609bb2f3dcc6208349fd11e07a0a3ca2fcce53f9
Scott M. Barbour <smbarbour@gmail.com>
2015-06-03 13:21:23 -0500

    Make sure that this works as a server-side only mod (no client needed)

--- src/main/java/mods/smbarbour/strangetoolkit/StrangeToolkit.java
+++ src/main/java/mods/smbarbour/strangetoolkit/StrangeToolkit.java
@@ -3,7 +3,11 @@ package mods.smbarbour.strangetoolkit;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.network.NetworkCheckHandler;
+import cpw.mods.fml.relauncher.Side;
 import mods.smbarbour.strangetoolkit.commands.OMTChangeOwnerCommand;
+
+import java.util.Map;
 
 @Mod(modid = "strangetoolkit", name="StrangeToolkit", version = "1.0")
 public class StrangeToolkit
@@ -13,5 +17,10 @@ public class StrangeToolkit
 		if (Loader.isModLoaded("openmodularturrets")) {
 			e.registerServerCommand(new OMTChangeOwnerCommand());
 		}
+	}
+
+	@NetworkCheckHandler
+	public boolean isAcceptable(Map<String,String> modList, Side side) {
+		return true;
 	}
 }
