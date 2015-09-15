195995874f5d6c64078170fa61736f73afcea998
Scott M. Barbour <smbarbour@gmail.com>
2015-09-15 15:52:32 -0500

    Removed OMT code (In OMT proper now) Added Animals+ spawn removal code.

--- src/main/java/mods/smbarbour/strangetoolkit/StrangeToolkit.java
+++ src/main/java/mods/smbarbour/strangetoolkit/StrangeToolkit.java
@@ -1,22 +1,23 @@
 package mods.smbarbour.strangetoolkit;
 
-import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.NetworkCheckHandler;
+import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.relauncher.Side;
-import mods.smbarbour.strangetoolkit.commands.OMTChangeOwnerCommand;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.BiomeGenBase;
 
 import java.util.Map;
 
-@Mod(modid = "strangetoolkit", name="StrangeToolkit", version = "1.0")
+@Mod(modid = "strangetoolkit", name="StrangeToolkit", version = "1.1")
 public class StrangeToolkit
 {
 	@Mod.EventHandler
 	public void serverStarting(FMLServerStartingEvent e) {
-		if (Loader.isModLoaded("openmodularturrets")) {
-			e.registerServerCommand(new OMTChangeOwnerCommand());
-		}
+		EntityRegistry.removeSpawn("Butterfly", EnumCreatureType.ambient, BiomeGenBase.getBiomeGenArray());
+		EntityRegistry.removeSpawn("Cricket", EnumCreatureType.ambient, BiomeGenBase.getBiomeGenArray());
+		EntityRegistry.removeSpawn("Moth", EnumCreatureType.ambient, BiomeGenBase.getBiomeGenArray());
 	}
 
 	@NetworkCheckHandler
