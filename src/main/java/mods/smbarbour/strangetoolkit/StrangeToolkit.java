feea553dd2c862992768e7d3e1c4e28bd4885f69
Scott M. Barbour <smbarbour@gmail.com>
2015-09-15 16:32:34 -0500

    Specified biomes directly

--- src/main/java/mods/smbarbour/strangetoolkit/StrangeToolkit.java
+++ src/main/java/mods/smbarbour/strangetoolkit/StrangeToolkit.java
@@ -15,9 +15,9 @@ public class StrangeToolkit
 {
 	@Mod.EventHandler
 	public void serverStarting(FMLServerStartingEvent e) {
-		EntityRegistry.removeSpawn("Butterfly", EnumCreatureType.ambient, BiomeGenBase.getBiomeGenArray());
-		EntityRegistry.removeSpawn("Cricket", EnumCreatureType.ambient, BiomeGenBase.getBiomeGenArray());
-		EntityRegistry.removeSpawn("Moth", EnumCreatureType.ambient, BiomeGenBase.getBiomeGenArray());
+		EntityRegistry.removeSpawn("Butterfly", EnumCreatureType.ambient, new BiomeGenBase[] { BiomeGenBase.plains, BiomeGenBase.swampland, BiomeGenBase.jungle, BiomeGenBase.jungleHills, BiomeGenBase.jungleEdge, BiomeGenBase.savanna, BiomeGenBase.savannaPlateau });
+		EntityRegistry.removeSpawn("Cricket", EnumCreatureType.ambient, new BiomeGenBase[] { BiomeGenBase.plains, BiomeGenBase.swampland, BiomeGenBase.jungle, BiomeGenBase.jungleHills, BiomeGenBase.jungleEdge, BiomeGenBase.savanna, BiomeGenBase.savannaPlateau });
+		EntityRegistry.removeSpawn("Moth", EnumCreatureType.ambient, new BiomeGenBase[] { BiomeGenBase.forest, BiomeGenBase.forestHills, BiomeGenBase.swampland, BiomeGenBase.taiga, BiomeGenBase.taigaHills, BiomeGenBase.birchForest, BiomeGenBase.birchForestHills, BiomeGenBase.roofedForest, BiomeGenBase.megaTaiga, BiomeGenBase.megaTaigaHills });
 	}
 
 	@NetworkCheckHandler
